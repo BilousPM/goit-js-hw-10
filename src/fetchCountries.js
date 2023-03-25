950b9b8587480e229124df41ca9c2098b403e928
Petro Bilous <bilouspm@gmail.com>
2023-03-25 21:37:50 +0200

    completes the task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,3 +4,14 @@
 //     return response.json();
 // })
 // };
+
+export function fetchCountries(country) {
+    const URL = `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
+    return fetch(URL).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        
+        return response.json();
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,76 +1,66 @@
 import './css/styles.css';
+import {fetchCountries} from '../src/fetchCountries'
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-// import {fetchCountries} from '../src/fetchCountries'
-const DEBOUNCE_DELAY = 1000;
+
+const DEBOUNCE_DELAY = 300;
 
 const refs = {
     inputEl: document.querySelector('#search-box'),
     countryListEl: document.querySelector('.country-list'),
-    countryInfoEl: document.querySelector('.country-info'),
-}
+    countryInfoEl: document.querySelector('.country-info')
+};
 
 refs.inputEl.addEventListener('input', debounce(hendleSearchInput, DEBOUNCE_DELAY));
 
 function hendleSearchInput(e) {
-    let countryName = (e.target.value);
-    fetchCountries(countryName)
-}
-
-
-
-function fetchCountries(country) {
-    const URL = `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
-    return fetch(URL).then(response => {
-        if (!response) {
-        throw new Error(response.status)
-        }
-        
-    return response.json();
-    }).then(country => {
-
-        const markInfo = markUpCountryInfo(country);
-        const markList = markUpCountryList(country);
-
-        if (country.length === 1) {
-            refs.countryListEl.innerHTML = markInfo
-            return
-        } else if (country.length > 1 && country.length <= 10) {
-            refs.countryListEl.innerHTML = markList
-            return
-        }
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        // console.log("Too many matches found. Please enter a more specific name.")
+    const countryName = e.target.value.trim();
     
-    // console.log(country.length);
-})
+    if (countryName === '') {
+        refs.countryListEl.innerHTML = '';
+        return;
+    }
+    fetchCountries(countryName).then(createsPageMarkup).catch(errorMessage);
 };
 
+function createsPageMarkup(country) {
+    const markList = markUpCountryList(country);
+    const markInfo = markUpCountryInfo(country);
+
+            if (country.length === 1) {
+                refs.countryListEl.innerHTML = markList
+                refs.countryInfoEl.innerHTML = markInfo
+                return;
+            } else if (country.length > 1 && country.length <= 10) {
+                refs.countryListEl.innerHTML = markList;
+                refs.countryInfoEl.innerHTML = '';
+                return;
+            }
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+}
+        
+function errorMessage(error) {
+     Notiflix.Notify.failure("Oops, there is no country with that name")
+}
 
 function markUpCountryList(array) {
-
     return array.map(({ flags, name, }) => {
         return `<div class="img_wraper"><img
         class="flag_img"
         src="${flags.svg}"
-        alt="${name.official}"
+        alt="${name.official}" width="50" height="20"
         />
-        <h3>${name.official}</h3>
-        </div>`  
-   }).join("")
-   
+        <h2>${name.official}</h2>
+        </div>`
+    }).join("");
 }
 
 function markUpCountryInfo(array) {
-    return array.map((count) => {
-        return `<img
-        class="flag_img"
-        src="${count.flags.svg}"
-        alt="${count.name.official}"
-        />
-        <h3>${count.name.official}</h3>
-        <p>Capital: ${count.capital}</p>
-        <p>Population: ${count.population}</p>
-        <p>Languages: ${count.languages}</p>`  
-   }).join("")
+    return array.map(({ capital, population, languages }) => {
+        return `
+        <p>Capital: ${capital}</p>
+        <p>Population: ${population}</p>
+        <p>Languages: ${Object.values(languages)}</p>`
+    }).join("");
 }
+
