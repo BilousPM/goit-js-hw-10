257e7a1a6ea7a0c54485848f9d4faafac282bf92
Petro Bilous <bilouspm@gmail.com>
2023-03-25 21:49:27 +0200

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,3 @@
-// export function fetchCountries(country) {
-//     let URL = "https://restcountries.com/v3.1/name/usa"
-// return fetch(URL).then(response => {
-//     return response.json();
-// })
-// };
-
 export function fetchCountries(country) {
     const URL = `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
     return fetch(URL).then(response => {
